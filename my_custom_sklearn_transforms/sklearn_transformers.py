d742038a5aeac228f8eb73ec0796ceace0c43c93
Isaac <isaacgonzalesv@gmail.com>
2020-08-19 17:22:42 -0300

    add custom sklearn

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,19 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+class AddMean(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        #data["NOTA_MEAN"]= data[self.columns].mean(axis=1, skipna=True)
+        data["NOTA_MEAN"]= data[self.columns].mean(axis=1, skipna=True)
+        data["TAREF_DIF"]=df_data_1["TAREFAS_ONLINE"]-df_data_1["FALTAS"]-df_data_1["REPROVACOES_DE"]-df_data_1["REPROVACOES_EM"]-df_data_1["REPROVACOES_GO"]-df_data_1["REPROVACOES_MF"]
+        return data
