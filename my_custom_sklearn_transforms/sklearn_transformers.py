acf1f716d94a257a32c8dfce3fbd2251403e3344
Isaac <isaacgonzalesv@gmail.com>
2020-08-19 22:02:11 -0300

    fix errors

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -6,9 +6,9 @@ class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns, add_mean, faltas, reprovad, mean_humanas):
         self.columns = columns
         self.add_mean = add_mean
-	self.faltas = faltas
-	self.reprovad = reprovad
-	self.mean_humanas = mean_humanas
+        self.faltas = faltas
+        self.reprovad = reprovad
+        self.mean_humanas = mean_humanas
 
     def fit(self, X, y=None):
         return self
@@ -18,11 +18,11 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         data["NOTA_MEAN"] = data[self.add_mean].mean(axis=1, skipna=True)
-	data["TOTAL_FALTAS"] = data[self.faltas].sum(axis=1, skipna=True)
-	data["TOTAL_REPROV"] = data[self.reprovad].sum(axis=1, skipna=True)
+        data["TOTAL_FALTAS"] = data[self.faltas].sum(axis=1, skipna=True)
+        data["TOTAL_REPROV"] = data[self.reprovad].sum(axis=1, skipna=True)
         data["TAREF_DIF"] = data["EXERCICIOS"]-data["TOTAL_FALTAS"]-data["TOTAL_REPROV"]
-	data["MEAN_H"] = data[self.mean_humanas].sum(axis=1, skipna=True)
-	self.columns.extend(["TOTAL_FALTAS", "TOTAL_REPROV"])
+        data["MEAN_H"] = data[self.mean_humanas].sum(axis=1, skipna=True)
+        self.columns.extend(["TOTAL_FALTAS", "TOTAL_REPROV"])
         return data.drop(labels=self.columns, axis='columns')
 
 
