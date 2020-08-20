31d9fb35f8d1352e8e4174ed6403627b10ac7a8d
Isaac <isaacgonzalesv@gmail.com>
2020-08-19 21:48:55 -0300

    add features

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -3,15 +3,12 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
-    def __init__(self, columns, add_mean, faltas, horas, reprovad, mean_humanas, hrs_humanas, rp_humanas):
+    def __init__(self, columns, add_mean, faltas, reprovad, mean_humanas):
         self.columns = columns
         self.add_mean = add_mean
 	self.faltas = faltas
-	self.horas = horas
 	self.reprovad = reprovad
 	self.mean_humanas = mean_humanas
-	self.hrs_humanas = hrs_humanas
-	self.rp_humanas = rp_humanas
 
     def fit(self, X, y=None):
         return self
@@ -24,6 +21,7 @@ class DropColumns(BaseEstimator, TransformerMixin):
 	data["TOTAL_FALTAS"] = data[self.faltas].sum(axis=1, skipna=True)
 	data["TOTAL_REPROV"] = data[self.reprovad].sum(axis=1, skipna=True)
         data["TAREF_DIF"] = data["EXERCICIOS"]-data["TOTAL_FALTAS"]-data["TOTAL_REPROV"]
+	data["MEAN_HUMANAS"] = data[self.reprovad].sum(axis=1, skipna=True)
         return data.drop(labels=self.columns, axis='columns')
 
 
